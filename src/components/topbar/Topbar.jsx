b2550186e1a42c193e466551599ce1cfecabce54
Anerie Patel <aneriep@gmail.com>
2024-06-27 17:23:24 -0400

    finished topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,7 @@
 import "./topbar.scss"
 import logo from "./logo.png"
+import linkedin from "./linkedin.png"
+import github from "./github.png"
 
 /*left: logo to go back to start, then skills, projects on the left, linkedin, github, gmail*/
 export default function Topbar() {
@@ -13,7 +15,24 @@ export default function Topbar() {
                 </div>
 
                 <div className="right">
-                    
+                    <a href="#skills" className="norm">
+                        Skills
+                    </a>
+
+                    <a href="#portfolio" className="norm">
+                        Projects
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/anerie-patel-962a75275/" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedin} alt="linkedin" className="logoFor" />
+                    </a>
+
+                    <a href="https://github.com/anerie-anerie" target="_blank" rel="noopener noreferrer">
+                        <img src={github} alt="github" className="logoFor" />
+                    </a>
+
+
+
                 </div>
             </div>
         </div>
