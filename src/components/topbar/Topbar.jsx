02500f332a6b06c6323b1b7ebaa8f1087f0bf426
Anerie Patel <aneriep@gmail.com>
2024-06-27 16:47:29 -0400

    image loaded

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,19 @@
 import "./topbar.scss"
+import logo from "./logo.png"
 
+/*left: logo to go back to start, then skills, projects on the left, linkedin, github, gmail*/
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">
-                        <img src="/logo.png" alt="Genius Logo" className="logoImage" />
+                        <img src={logo} alt="Anerie Patel" className="logoImage" />
                     </a>
                 </div>
 
                 <div className="right">
-                    this is right
+                    
                 </div>
             </div>
         </div>
