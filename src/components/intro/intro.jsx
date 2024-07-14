f685182a94bb517c29e2043cab5050def4661c29
Anerie Patel <aneriep@gmail.com>
2024-07-14 18:28:08 -0300

    testing

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,7 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            hi! I'm anerie
+            hi! I'm anerie lets see how this works
 
         </div>
 
