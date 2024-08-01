6976d7c5989f49ad2a5a700b3770f2ace3dd4865
Anerie Patel <aneriep@gmail.com>
2024-08-01 15:34:37 -0400

    testing

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,8 @@ export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
             here are some projects!
-
+            my favourite is called Posture Perfect!
+            
         </div>
 
     )
