156ef4f6f708793d81f7753c2d84d77fff035c26
Anerie Patel <aneriep@gmail.com>
2024-08-03 00:24:02 -0400

    animated text

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,12 +1,29 @@
-import "./intro.scss"
+import "./intro.scss";
+import { ReactTyped as Typed } from 'react-typed';
 
 export default function Intro() {
     return (
-        <div className="intro" id="intro">
-            hi! I'm anerie lets see how this works
-
+        <div className="fullIntro">
+            {/* Introduction Text */}
+            <div className="intro" id="intro">
+                hi! I'm anerie lets see how this works
+            </div>
+            
+            {/* Animated Typing Text */}
+            <div className="animated-typing">
+                <Typed
+                    strings={[
+                        "I'm an artist",
+                        "I'm a student",
+                        "I'm a coder",
+                        "I'm a creative",
+                        "I'm a team player"
+                    ]}
+                    typeSpeed={150}  // Speed for typing
+                    backSpeed={100}  // Speed for deleting the words
+                    loop             // Looping the animation
+                />
+            </div>
         </div>
-
-    )
-
-} 
+    );
+}
