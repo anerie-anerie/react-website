f81f2bf042ae32dc2d564a3beddd1b5fc1bfabed
Anerie Patel <aneriep@gmail.com>
2024-08-16 15:52:02 -0400

    rectangle testing

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,25 +4,31 @@ import { ReactTyped as Typed } from 'react-typed';
 export default function Intro() {
     return (
         <div className="fullIntro">
-            {/* Introduction Text */}
-            <div className="intro" id="intro">
-                hi! I'm anerie lets see how this works
-            </div>
-            
-            {/* Animated Typing Text */}
-            <div className="animated-typing">
-                <Typed
-                    strings={[
-                        "I'm an artist",
-                        "I'm a student",
-                        "I'm a coder",
-                        "I'm a creative",
-                        "I'm a team player"
-                    ]}
-                    typeSpeed={150}  // Speed for typing
-                    backSpeed={100}  // Speed for deleting the words
-                    loop             // Looping the animation
-                />
+            {/* Background Rectangle */}
+            <div className="background-rectangle">
+                {/* Rectangle for Intro Texts */}
+                <div className="text-rectangle">
+                    {/* Introduction Text */}
+                    <div className="intro" id="intro">
+                        hi! I'm anerie lets see how this works
+                    </div>
+                    
+                    {/* Animated Typing Text */}
+                    <div className="animated-typing">
+                        <Typed
+                            strings={[
+                                "I'm an artist",
+                                "I'm a student",
+                                "I'm a coder",
+                                "I'm a creative",
+                                "I'm a team player"
+                            ]}
+                            typeSpeed={150}  // Speed for typing
+                            backSpeed={100}  // Speed for deleting the words
+                            loop             // Looping the animation
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
