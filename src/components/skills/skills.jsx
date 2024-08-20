d8a7bb6594de2871f04e894722320c146c6cf23f
Anerie Patel <aneriep@gmail.com>
2024-08-20 10:15:03 -0400

    next page start

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -3,8 +3,9 @@ import "./skills.scss"
 export default function Skills() {
     return (
         <div className="skills" id="skills">
+            <div className="text-rect">
             these are my skills:
-
+            </div>
         </div>
 
     )
