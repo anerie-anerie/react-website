df5a82e0dfbcc62339c50e05f22674d51d4b81f5
Anerie Patel <aneriep@gmail.com>
2024-11-21 18:14:42 -0500

    newchange

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,31 @@
 import "./portfolio.scss"
 
+/*
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
             here are some projects!
-            my favourite is called Posture Perfect!
-            
+            my fav is called Posture Perfect!
+            <a href="https://github.com/anerie-anerie" target="_blank" rel="noopener noreferrer">
+            </a>
         </div>
 
     )
+}*/
 
-}
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/api')
+            .then(response => setMessage(response.data.message))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return <div>{message}</div>;
+};
+
+export default App;
